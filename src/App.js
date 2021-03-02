9bcb091920956c64c17d83cba5bc15653dd95539
vladosxxx <vladosxxx@gmail.com>
2021-03-02 17:13:46 +0200

    start todo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import React, {useState, useEffect} from 'react'
 import './App.css';
-import TimerComponent from './components/TimerComponent'
+import {useState, useEffect} from 'react'
+import { interval } from 'rxjs'
+import { map } from 'rxjs/operators'
+// import { $ } from "react-rxjs-elements";
 
 function App() {
-    const [seconds, setSeconds] = useState(0)
-    const [minutes, setMinutes] = useState(0)
-    const [hours, setHours] = useState(0)
+    const [seconds, setSeconds] = useState(300)
+    const [isSecondsShow, setSecondsShow] = useState()
+    const [minutes, setMinutes] = useState()
+    const [hours, setHours] = useState()
 
+    const [isStart, setStart] = useState(false)
+
+    const startStop = () => {
+        setStart(!isStart)
+        setSeconds(300)
+    }
+    const reset = () => {
+        setStart(true)
+    }
+    const wait = () => {
+        setStart(!isStart)
+    }
     useEffect(() => {
+        setHours(Math.floor(seconds / 60 / 60))
+        setMinutes(Math.floor(seconds / 60))
+        setSecondsShow(seconds % 60)
+        if(isStart === true)
+        {
 
-    })
+            const stream$ = interval(1000).pipe(map(i => seconds - i))
+            const subscription = stream$
+                .subscribe(i =>
+                    setSeconds(i)
+
+                );
+
+            return () => subscription.unsubscribe();
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isStart] )
+
+
   return (
     <div className="App">
-
-      <TimerComponent/>
+        <span>hr{hours}</span>
+        <span>min{minutes}</span>
+        <span>sec{isSecondsShow}</span>
+        <button onClick={startStop}>start/stop</button>
+        <button onClick={reset}>reset</button>
+        <button onClick={wait}>reset</button>
     </div>
   );
 }
